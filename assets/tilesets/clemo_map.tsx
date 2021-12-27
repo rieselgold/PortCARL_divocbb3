8fd870e4a7039944c00f69748b0d72e34fa8dda1
Clemens Ehlich <rod.fluesop@gmail.com>
2021-12-27 22:21:01 +0100

    Phantoland goes Online!

---
+++ assets/tilesets/clemo_map.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="map" tilewidth="32" tileheight="32" tilecount="550" columns="55">
+ <image source="map.png" trans="ffffff" width="1791" height="320"/>
+</tileset>
