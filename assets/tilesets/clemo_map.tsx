aa3c687dbb2c1068bc39786ba5a3d423c8f1c71a
Clemens Ehlich <rod.fluesop@gmail.com>
2021-12-27 22:40:31 +0100

    Update clemo_map.tsx

--- assets/tilesets/clemo_map.tsx
+++ assets/tilesets/clemo_map.tsx
@@ -1,4 +1,9 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.5" tiledversion="1.7.2" name="map" tilewidth="32" tileheight="32" tilecount="550" columns="55">
  <image source="map.png" trans="ffffff" width="1791" height="320"/>
+ <tile id="0">
+  <properties>
+   <property name="tilesetCopyright" value="CC0"/>
+  </properties>
+ </tile>
 </tileset>
