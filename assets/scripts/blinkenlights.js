54cf3f4ef8cec17ce631020c3577232048ab7629
Mischosch <michael@wesrc.com>
2021-12-28 04:49:26 +0100

    handle 3 animations

--- assets/scripts/blinkenlights.js
+++ assets/scripts/blinkenlights.js
@@ -1,6 +1,7 @@
 WA.onInit().then(() => {
 
     WA.room.hideLayer('obj_haus_vordergrund_pac');
+    WA.room.hideLayer('obj_haus_vordergrund_ccc');
     WA.room.showLayer('obj_haus_vordergrund_herz');
 
     WA.chat.sendChatMessage("Hello!", 'Mr Robot');
@@ -8,21 +9,17 @@ WA.onInit().then(() => {
     WA.room.onEnterLayer('blinkenlights_switch_1').subscribe(() => {
         WA.room.showLayer('obj_haus_vordergrund_pac');
         WA.room.hideLayer('obj_haus_vordergrund_herz');
-        //WA.room.hideLayer('obj_haus_animation3');
     });
     WA.room.onLeaveLayer('blinkenlights_switch_1').subscribe(() => {
         WA.room.hideLayer('obj_haus_vordergrund_pac');
         WA.room.showLayer('obj_haus_vordergrund_herz');
-        //WA.room.showLayer('obj_haus_animation3');
     });
     WA.room.onEnterLayer('blinkenlights_switch_2').subscribe(() => {
-        WA.room.hideLayer('obj_haus_animation1');
-        WA.room.showLayer('obj_haus_animation2');
-        WA.room.hideLayer('obj_haus_animation3');
+        WA.room.showLayer('obj_haus_vordergrund_ccc');
+        WA.room.hideLayer('obj_haus_vordergrund_herz');
     });
     WA.room.onLeaveLayer('blinkenlights_switch_2').subscribe(() => {
-        WA.room.showLayer('obj_haus_animation1');
-        WA.room.showLayer('obj_haus_animation2');
-        WA.room.showLayer('obj_haus_animation3');
+        WA.room.hideLayer('obj_haus_vordergrund_ccc');
+        WA.room.showLayer('obj_haus_vordergrund_herz');
     });
 });
