750e2cbdd67f8885e6923134a617af1d47fcd815
Mischosch <michael@wesrc.com>
2021-12-28 01:12:36 +0100

    show/hide layer test

--- assets/scripts/blinkenlights.js
+++ assets/scripts/blinkenlights.js
@@ -1,8 +1,10 @@
 WA.onInit().then(() => {
     WA.room.onEnterLayer('blinkenlights_switch_1').subscribe(() => {
         WA.chat.sendChatMessage("Hello!", 'Mr Robot');
+        WA.room.hideLayer('obj_haus_vordergrund');
     });
     WA.room.onLeaveLayer('blinkenlights_switch_1').subscribe(() => {
         WA.chat.sendChatMessage("Goodbye!", 'Mr Robot');
+        WA.room.showLayer('obj_haus_vordergrund');
     });
 });
