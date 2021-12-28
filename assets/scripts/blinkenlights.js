fcdfa386cba05c0f230de38eb0c987c3be8b9790
Mischosch <michael@wesrc.com>
2021-12-28 03:20:50 +0100

    add more blinkenlights change effects

--- assets/scripts/blinkenlights.js
+++ assets/scripts/blinkenlights.js
@@ -1,10 +1,26 @@
 WA.onInit().then(() => {
     WA.room.onEnterLayer('blinkenlights_switch_1').subscribe(() => {
         WA.chat.sendChatMessage("Hello!", 'Mr Robot');
-        WA.room.hideLayer('obj_haus_animation1');
+        WA.room.showLayer('obj_haus_animation1');
+        WA.room.hideLayer('obj_haus_animation2');
+        WA.room.hideLayer('obj_haus_animation3');
     });
     WA.room.onLeaveLayer('blinkenlights_switch_1').subscribe(() => {
         WA.chat.sendChatMessage("Goodbye!", 'Mr Robot');
+        WA.room.hideLayer('obj_haus_animation1');
+        WA.room.showLayer('obj_haus_animation2');
+        WA.room.showLayer('obj_haus_animation3');
+    });
+    WA.room.onEnterLayer('blinkenlights_switch_2').subscribe(() => {
+        WA.chat.sendChatMessage("Hello!", 'Mr Robot');
+        WA.room.hideLayer('obj_haus_animation1');
+        WA.room.showLayer('obj_haus_animation2');
+        WA.room.hideLayer('obj_haus_animation3');
+    });
+    WA.room.onLeaveLayer('blinkenlights_switch_2').subscribe(() => {
+        WA.chat.sendChatMessage("Goodbye!", 'Mr Robot');
         WA.room.showLayer('obj_haus_animation1');
+        WA.room.showLayer('obj_haus_animation2');
+        WA.room.showLayer('obj_haus_animation3');
     });
 });
