9f01ca73fa5acbddaba574f436c13cdf21e3132c
Mischosch <michael@wesrc.com>
2021-12-28 04:56:12 +0100

    dont use chat system

--- assets/scripts/blinkenlights.js
+++ assets/scripts/blinkenlights.js
@@ -4,8 +4,6 @@ WA.onInit().then(() => {
     WA.room.hideLayer('obj_haus_vordergrund_ccc');
     WA.room.showLayer('obj_haus_vordergrund_herz');
 
-    WA.chat.sendChatMessage("Hello!", 'Mr Robot');
-
     WA.room.onEnterLayer('blinkenlights_switch_1').subscribe(() => {
         WA.room.showLayer('obj_haus_vordergrund_pac');
         WA.room.hideLayer('obj_haus_vordergrund_herz');
