e207977db10dc15865d53f8a1b41b02c9e689792
Mischosch <michael@wesrc.com>
2021-12-28 01:49:44 +0100

    update blinken effect trigger

--- assets/scripts/blinkenlights.js
+++ assets/scripts/blinkenlights.js
@@ -1,10 +1,10 @@
 WA.onInit().then(() => {
     WA.room.onEnterLayer('blinkenlights_switch_1').subscribe(() => {
         WA.chat.sendChatMessage("Hello!", 'Mr Robot');
-        WA.room.hideLayer('obj_haus_vordergrund');
+        WA.room.hideLayer('obj_haus_animation1');
     });
     WA.room.onLeaveLayer('blinkenlights_switch_1').subscribe(() => {
         WA.chat.sendChatMessage("Goodbye!", 'Mr Robot');
-        WA.room.showLayer('obj_haus_vordergrund');
+        WA.room.showLayer('obj_haus_animation1');
     });
 });
